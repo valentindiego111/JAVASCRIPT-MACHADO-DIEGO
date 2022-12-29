02b420b3a803be32db0b417e30cdf3f9c4df8d08
DiegoMachado <diegovalentinmachado1@gmail.com>
2022-12-29 20:18:34 -0300

    Primer entrega

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log ("BIENVENIDOS")
+alert ("bienvenidos")
+
+let pedido = prompt ("ingrese su pedido")
+console.log ("el pedido que usted ordeno es " + pedido )
+
+let bebida = prompt ("Ingrese Su bebida")
+
+console.log ("Su bebida es " + bebida )
+
+
+alert ( pedido + " y " + bebida )
+
+
+
+
+
+
+let num =1; let dato;
+while (num<10)
+{
+dato=prompt('Introduce número menor que 10:','');
+num=parseInt(dato);
+
+document.write(` ${num} <br> `);
+} 
+
